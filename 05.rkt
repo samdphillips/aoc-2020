f84cdfd27f7f23cf746d85110d5bc64691f083ee
Sam Phillips <samdphillips@gmail.com>
2020-12-04 21:43:33 -0800

    [day 5] part 1 done

---
+++ 05.rkt
@@ -0,0 +1,26 @@
+#lang racket/base
+
+(require racket/match)
+
+(module+ test
+  (require rackunit))
+
+(define (decode-seat-id s)
+  (for/fold ([seat-id 0]) ([ch (in-string s)])
+    (+ (* 2 seat-id)
+       (match ch
+         [(or #\F #\L) 0]
+         [(or #\B #\R) 1]))))
+
+(module+ test
+  (check-equal? (decode-seat-id "BFFFBBFRRR") 567)
+  (check-equal? (decode-seat-id "FFFBBBFRRR") 119)
+  (check-equal? (decode-seat-id "BBFFBBFRLL") 820))
+
+(module* part-1 #f
+  (require racket/port)
+  (call-with-input-file "inputs/05.txt"
+    (lambda (inp)
+      (for/fold ([v 0]) ([seat (in-lines inp)])
+        (max v (decode-seat-id seat))))))
+
